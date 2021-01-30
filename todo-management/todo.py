d0e3f50a0779d9ed0f92ab50d5b77c430f797069
ProCode2 <bpro249@gmail.com>
2021-01-30 14:48:51 +0530

    update todo.py

--- todo-management/todo.py
+++ todo-management/todo.py
@@ -6,22 +6,53 @@ choice = input(help_text + "\n> ")
 
 if choice == "1":
     # open the file
-    file = open("todo.txt", "r+")
+    try:
+        # check if the file exists
+        file = open("todo.txt", "r+")
+    except:
+        # if the file does not exist
+        print("No todos exists at the moment.")
+        exit()
+
+    # get the todos as an array
     todos = file.read().split("\n")
-    print(todos)
+    # remove empty strings from the array
+    todos.remove('')
+
+    # if there are no todos
+    if len(todos) == 0:
+        print("No todos exists at the moment.")
+        exit()
+
+    # prints the todos
+    for i in range(0, len(todos)):
+        print(f"[{i + 1}] {todos[i]}")
+
+    # close the file
     file.close()
+
 elif choice == "2":
+    # get the todo to add
     todo = input("Enter the todo to add: ")
+    # open the file in append mode
     file = open("todo.txt", "a")
+    # write the todo to the file
     file.write(todo + "\n")
+    # close the file
     file.close()
+
 elif choice == "3":
     # get the todos
     file = open("todo.txt", "r+")
     todos = file.read().split("\n")
+    todos.remove('')
+
+    # print the todos  once again
     for i in range(0, len(todos)):
         print(f"[{i + 1}] {todos[i]}")
+    # close file
     file.close()
+    # open the file
     file = open("todo.txt", "w")
     # get the todo number to be deleted
     todo_number = int(input("Enter the todo number to be deleted: "))
@@ -29,7 +60,7 @@ elif choice == "3":
     for i in range(0, len(todos)):
         file.write(todos[i] + "\n")
     file.close()
+
 else:
     print("That is not a valid option.")
     print(help_text)
-print(choice)
